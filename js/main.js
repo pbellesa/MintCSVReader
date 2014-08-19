e0d70b801194be14b0bbdc675476bdf054b626c6
Pedro Bellesa <bspedropaulo@myseneca.ca>
2014-08-18 21:08:24 -0400

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// Application Code 
+
+
+
+
+
+// Angular JS Code
+(function () {
+var app = angular.module('transactionList', []);
+
+app.controller('CSVController', function ($scope) {
+    this.data = $scope.data;
+    this.show = -1;
+    this.toObject = function () {
+        try {
+            this.results = $scope.results;
+            
+            this.error = "";
+        } catch (e) {
+            console.log(e);
+            this.error = "Input CSV data.";
+        }
+    };
+    this.setRowClass = function (transactionType) {
+        if (transactionType == "debit") {
+            return "panel-danger";
+        } else if (transactionType == "credit") {
+            return "panel-success";
+        }
+    };
+
+
+    $scope.setData = function () {
+        var reader = new FileReader();
+        reader.readAsText($scope.file);
+        reader.onload = function (event) {
+            this.data = event.target.result;
+            $scope.results = $.csv.toObjects(this.data);
+            $scope.$apply();
+            console.log(this.results);
+        };
+        reader.onerror = function () { alert('Unable to read ' + file.fileName); };
+    };
+});
+
+app.directive("ngFileSelect", function () {
+    return {
+        link: function ($scope, el) {
+            el.bind("change", function (e) {
+                $scope.file = (e.srcElement || e.target).files[0];
+                $scope.$apply("setData()");
+            });
+        }
+    }
+});
+
+
+})();
+
+
